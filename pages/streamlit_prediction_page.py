1c98cb464de126dfee111328c32ad2e2e73c7afa
tmn1601 <47769179+tam-nguyenminh@users.noreply.github.com>
2024-07-29 15:18:43 +0700

    Update streamlit_prediction_page.py

--- pages/streamlit_prediction_page.py
+++ pages/streamlit_prediction_page.py
@@ -5,40 +5,29 @@ import json
 import pickle
 
 
-st.title('Room-for-Rent Price Prediction')
-st.subheader('This is to predict room renting price in Ho Chi Minh city. ', 
-        help='help icon',
-        divider='blue')
 
-st.markdown('Estimator used in this training is :green[RandomForest].:sunflower:')
-
+path = 'C:/Users/84393/OneDrive - CÔNG TY TNHH OFFICIENCE/PYTHON/ML_House_Rent/'
 
 
 # import list of districts
-# path = 'C:/Users/84393/OneDrive - CÔNG TY TNHH OFFICIENCE/PYTHON/ML_House_Rent/'
-f = open('columns.json')
-features = json.load(f)
-feature_list = features['data_columns']
-dist_list = features['data_columns'][1:]
+def load_features():
+    
+    f = open(path + 'columns.json')
+    features = json.load(f)
+    feature_list = features['data_columns']
+    dist_list = features['data_columns'][1:]
+    return feature_list, dist_list
 # print(dist_list)
 
-st.markdown(f'\n')
-
-# create option list of district
-st.subheader('Select a district.')
-dist_slt = st.selectbox(f'Now select a district', dist_list)
-
-
-# st.markdown('#')
-
-# create slider of square metre
-st.subheader('Select a square metre value.')
-sqrmetre_slt = st.slider('Select an square metre value.', min_value=1, max_value=50, step=1)
-st.write("Selected square metre", sqrmetre_slt)
+feature_list, dist_list = load_features()
 
 # add model
-with open('hcm_room_price_prediction_model.pickle', 'rb') as f:
-    model = pickle.load(f)
+def load_model():
+    with open(path + 'hcm_room_price_prediction_model.pickle', 'rb') as f:
+        model = pickle.load(f)
+    return model 
+
+model = load_model()
 
 def predict_price(district, sqr_meter):
     district_index = feature_list.index(district)
@@ -50,14 +39,28 @@ def predict_price(district, sqr_meter):
         x[district_index] = 1
     return x.reshape(1,-1)
 
-# print(dist_list)
-# new_value = predict_price('quận 1', 45)
-new_value = predict_price(dist_slt, sqrmetre_slt)
+def show_predict_page():
+    st.title('Room-for-Rent Price Prediction')
+    st.subheader('This is to predict room renting price in Ho Chi Minh city. ', 
+            help='help icon',
+            divider='blue')
 
-predicted_val = model.predict(new_value)[0]
-# print(predicted_val)
+    st.markdown('Estimator used in this training is :green[RandomForest].:sunflower:')
+    # create option list of district
+    st.subheader('Select a district.')
+    dist_slt = st.selectbox(f'Now select a district', dist_list)
 
-# print(type(predicted_val))
 
-st.subheader(f'Predicted price (million VND): ')
-st.header(f':red[{predicted_val}]')
+    st.markdown(f'\n')
+
+    # create slider of square metre
+    st.subheader('Select a square metre value.')
+    sqrmetre_slt = st.slider('Select an square metre value.', min_value=1, max_value=50, step=1)
+    st.write("Selected square metre", sqrmetre_slt)
+
+    new_value = predict_price(dist_slt, sqrmetre_slt)
+
+    predicted_val = model.predict(new_value)[0]
+
+    st.subheader(f'Predicted price (million VND): ')
+    st.header(f':red[{predicted_val}]')
