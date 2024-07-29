60e467212e74e90e772463a3c2c96ce62154b477
tmn1601 <47769179+tam-nguyenminh@users.noreply.github.com>
2024-07-29 15:37:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import streamlit as st
-from streamlit_prediction_page import show_predict_page
-from pages.about_page import show_about_page
+# from streamlit_prediction_page import show_predict_page
+# from pages.about_page import show_about_page
 
-pg = st.navigation([
-    st.Page(page="pages/streamlit_prediction_page.py", title='Prediction', icon=':material/experiment:'), 
-    st.Page(page='pages/about_page.py', title='About Dataset', icon=':material/info:', url_path='About-info')
-    ])
+st.title('home page')
+# pg = st.navigation([
+#     st.Page(page="pages/streamlit_prediction_page.py", title='Prediction', icon=':material/experiment:'), 
+#     st.Page(page='pages/about_page.py', title='About Dataset', icon=':material/info:', url_path='About-info')
+#     ])
 
-if pg.title == 'Prediction':
-    show_predict_page()
-else:
-    show_about_page()
+# if pg.title == 'Prediction':
+#     show_predict_page()
+# else:
+#     show_about_page()
 
 
-pg.run()
+# pg.run()
