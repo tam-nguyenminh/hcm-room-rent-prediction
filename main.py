cbaaec7790cfc00a4fe24b0b18c490bcc775470c
tmn1601 <47769179+tam-nguyenminh@users.noreply.github.com>
2024-07-29 15:21:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from pages.about_page import show_about_page
 
 pg = st.navigation([
     st.Page(page="streamlit_prediction_page.py", title='Prediction', icon=':material/experiment:'), 
-    st.Page(page='pages/About_page.py', title='About Dataset', icon=':material/info:', url_path='About-info')
+    st.Page(page='pages/about_page.py', title='About Dataset', icon=':material/info:', url_path='About-info')
     ])
 
-print(pg)
 if pg.title == 'Prediction':
     show_predict_page()
 else:
