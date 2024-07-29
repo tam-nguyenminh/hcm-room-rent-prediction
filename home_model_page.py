740b73cce6f8184d6252f307716259316505d469
tmn1601 <47769179+tam-nguyenminh@users.noreply.github.com>
2024-07-29 15:18:56 +0700

    Update and rename home_model_page.py to main.py

--- home_model_page.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-pg = st.navigation([
-    st.Page(page="streamlit_prediction_page.py", title='Prediction', icon=':material/experiment:'), 
-    st.Page(page='pages/About.py', title='About Dataset', icon=':material/info:', url_path='About-info')
-    ])
-pg.run()
---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from streamlit_prediction_page import show_predict_page
+from pages.about_page import show_about_page
+
+pg = st.navigation([
+    st.Page(page="streamlit_prediction_page.py", title='Prediction', icon=':material/experiment:'), 
+    st.Page(page='pages/About_page.py', title='About Dataset', icon=':material/info:', url_path='About-info')
+    ])
+
+print(pg)
+if pg.title == 'Prediction':
+    show_predict_page()
+else:
+    show_about_page()
+
+
+# pg.run()st
