91063b8123f40bb259c50128f6aef074f29c704f
tmn1601 <47769179+tam-nguyenminh@users.noreply.github.com>
2024-07-29 15:18:29 +0700

    Update and rename About.py to about_page.py

--- pages/About.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st 
-
-st.title('Hello from About')
-
-st.text('Drawback: data information such as furniture, room status etc., facility neighborhood is not provided.')
---
+++ pages/about_page.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+
+def show_about_page():
+    st.title('Hello from About')
+    st.text('Drawback: data information such as furniture, room status etc., facility neighborhood is not provided.')
